6c955f78575b8be98b4dfb4c71c4c0f68b960e07
neroli <50916322+Psmths@users.noreply.github.com>
2021-11-15 17:50:28 -0600

    new opcodes

--- include/structures.hpp
+++ include/structures.hpp
@@ -5,6 +5,7 @@
 #include <stdint.h>
 
 typedef uint8_t cpu_byte;
+typedef uint16_t cpu_halfword;
 typedef uint32_t cpu_word;
 typedef uint64_t cpu_dword;
 
--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,18 @@ int main() {
     /* currently very rudimetary unit test */
     CPU *my_cpu = new CPU();
 
-    cpu_word rom[11] = {0x00000000, 0x00000011, 0x00000021, 0x00000021,
-                        0x00000021, 0x00000011, 0x00000000, 0x00000000,
-                        0x00000000, 0x00000000, 0x00000001};
+    /* Simple loop program
+
+    MOVE_LOWER R0011, 0000000000000010
+    MOVE_LOWER R0001, 0000000000001111
+    INCREMENT R0000
+    COMPARE R0000, R0001
+    JUMP_NE R0011
+    HALT
+
+    */
+    cpu_word rom[11] = {0x00130002, 0x0011000F, 0x00000020, 0x00000110,
+                        0x00000073, 0x00000001};
 
     my_cpu->load_rom(rom, 11*sizeof(cpu_word));
     my_cpu->run();
